bdf462c55ba7bd5f4ad8f13db686ec8ead4b6773
xcusive007x <143580398+xcusive007x@users.noreply.github.com>
2025-04-18 02:51:55 +0700

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,8 +1,8 @@
 local gameId = game.PlaceId
 if gameId == 72829404259339 then
 	print("anime rangers x")
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/xcusive007x/xyz/refs/heads/main/arx.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/xcusive007x/xyz/refs/heads/main/arxob.lua"))()
 	elseif gameId == 8304191830 then
 	print("anime adventure")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/xcusive007x/xyz/refs/heads/main/aa.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/xcusive007x/xyz/refs/heads/main/aaob.lua"))()
 end
